94d3d5fffd10211ad66e1128a102496e3c85dc53
AUMSTER <74236586+aumsaur@users.noreply.github.com>
2023-03-24 09:50:48 +0700

    Revert "Update Api script"
    This reverts commit 4a57512fd3e296ac654948b9207d3afc109ced99.

--- main.py
+++ main.py
@@ -1,24 +1,10 @@
-from fastapi import FastAPI
-# from starlette.middleware.cors import CORSMiddleware
+from apps.auth import app
+import ssl
 
-# from apps.api.v1.auth import router as auth_router
-# from apps.core.config import settings
-from apps.api.v1.read.service import router
+# Generate a self-signed SSL/TLS certificate
+context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+context.load_cert_chain(certfile="./perm/cert.pem", keyfile="./perm/key.pem")
 
-# app = FastAPI(
-#     title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
-# )
-app = FastAPI()
-# Set all CORS enabled origins
-# if settings.BACKEND_CORS_ORIGINS:
-#     app.add_middleware(
-#         CORSMiddleware,
-#         allow_origins=[str(origin)
-#                        for origin in settings.BACKEND_CORS_ORIGINS],
-#         allow_credentials=True,
-#         allow_methods=["*"],
-#         allow_headers=["*"],
-#     )
-
-# app.include_router(auth_router, prefix=settings.API_V1_STR)
-app.include_router(router)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000, ssl=context)
