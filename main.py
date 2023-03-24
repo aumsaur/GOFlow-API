677ffccc0f2f0b604984141ad58b20e51bba6995
AUMSTER <74236586+aumsaur@users.noreply.github.com>
2023-03-24 09:51:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from apps.auth import app
+from auth import app
 import ssl
 
 # Generate a self-signed SSL/TLS certificate
@@ -6,5 +6,4 @@ context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
 context.load_cert_chain(certfile="./perm/cert.pem", keyfile="./perm/key.pem")
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, ssl=context)
+    app.run()
