73432f2d0dbe378d349f2465e79c2db913660634
AUMSTER <74236586+aumsaur@users.noreply.github.com>
2023-03-24 09:59:11 +0700

    Revert google api

--- db/database.py
+++ db/database.py
@@ -1,15 +1,15 @@
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
-from sqlalchemy.orm import sessionmaker
+# from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+# from sqlalchemy.orm import sessionmaker
 
-# Define database information
-engine = create_engine('sqlite:///mockup_azure_sql.db')
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# # Define database information
+# engine = create_engine('sqlite:///mockup_azure_sql.db')
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-metadata = MetaData()
+# metadata = MetaData()
 
-# Define the table structure
-users = Table('users', metadata,
-              Column('id', Integer, primary_key=True),
-              Column('username', String),
-              Column('email', String),
-              Column('password', String))
+# # Define the table structure
+# users = Table('users', metadata,
+#               Column('id', Integer, primary_key=True),
+#               Column('username', String),
+#               Column('email', String),
+#               Column('password', String))
