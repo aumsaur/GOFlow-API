90ccf350cc8ac4b885713c9e53d97766eeeb9398
AUMSTER <74236586+aumsaur@users.noreply.github.com>
2023-03-28 19:19:03 +0700

    update model database

--- apps/models/item.py
+++ apps/models/item.py
@@ -1,18 +1,29 @@
-from typing import TYPE_CHECKING
+# from typing import TYPE_CHECKING
 
-from sqlalchemy import Boolean, Column, JSON, String
+from sqlalchemy import Column, ForeignKey, JSON, String, UniqueConstraint, ForeignKeyConstraint
 from sqlalchemy.orm import relationship
-
-from azure.base_class import Base
+from apps.azure.base_model import Base
+# from .user import User
 
 # if TYPE_CHECKING:
 #     from .item import Item  # noqa: F401
 
 
 class Item(Base):
-    uid = Column(String, primary_key=True, index=True, unique=True)
-    owner = Column(String, index=True)
-    title = Column(String)
-    created = Column(String)
-    edited = Column(String)
-    metadata = Column(JSON)
+    id = Column(String, primary_key=True, index=True, unique=True)
+    owner_id = Column(String, ForeignKey("user.id"), index=True)
+    # owner_email = Column(String, ForeignKey("user.email"), index=True)
+    owner = relationship("User", back_populates="items")
+    title = Column(String, index=False)
+    created = Column(String, index=False)
+    edited = Column(String, index=False)
+    itemdata = Column(JSON, index=False)
+
+    __table_args__ = (
+        ForeignKeyConstraint(
+            ['owner_id'],
+            ['user.id'],
+            name='item_owner_fk'
+        ),
+        UniqueConstraint('id', name='uq_item_id')
+    )
--- apps/models/user.py
+++ apps/models/user.py
@@ -1,45 +1,17 @@
-from typing import TYPE_CHECKING
+from sqlalchemy import Boolean, Column, String
+from sqlalchemy.orm import relationship
 
-from sqlalchemy import Boolean, Column, String, event
-# from sqlalchemy.event import listen
-from pydantic import validator
+# from pydantic import validator
 
-from apps.azure.base_class import Base
-
-# if TYPE_CHECKING:
-#     from .item import Item  # noqa: F401
+from apps.azure.base_model import Base
+# from schemas.user import UserType
 
 
 class User(Base):
-    email = Column(String, primary_key=True, unique=True,
-                   index=True, nullable=False)
-    type = Column(Boolean, index=True)  # True for App
-    displayname = Column(String, index=True)
-    hashed_password = Column(String, nullable=True)
+    id = Column(String, primary_key=True, index=False, nullable=False)
+    email = Column(String, primary_key=True, index=True, nullable=False)
+    type = Column(String, index=False)  # True for App
+    displayname = Column(String, index=False)
+    hashed_password = Column(String, nullable=True, index=False)
 
-    def __init__(self, email, type, displayname, hashed_password=None):
-        self.email = email
-        self.type = type
-        self.displayname = displayname
-        self.hashed_password = hashed_password
-
-    @staticmethod
-    def hashed_password_nullable(context):
-        if not context['type']:
-            return None
-        else:
-            return context.current_parameters['hashed_password']
-
-    @validator('hashed_password')
-    def validate_hashed_password(self, key, value):
-        if not self.type:
-            return None
-        else:
-            return value
-
-    # Define the before_insert event
-    @event.listens_for(Base, 'before_insert')
-    def before_insert_listener(mapper, connection, target):
-        # Validate the hashed_password before insert
-        target.hashed_password = target.validate_hashed_password(
-            'hashed_password', target.hashed_password)
+    items = relationship("Item", back_populates="owner")
