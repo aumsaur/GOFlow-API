4d78e3fccf5b6fe42145b551c75f1f87b918f902
AUMSTER <74236586+aumsaur@users.noreply.github.com>
2023-03-28 19:20:04 +0700

    update crud

--- apps/crud/crud_item.py
+++ apps/crud/crud_item.py
@@ -1,61 +1,83 @@
 from typing import (
     Union,
     Optional,
+    List,
+    List,
     Dict,
     Any
 )
 
 from .crud_base import CRUDBase
 from apps.models.item import Item
-from schemas.item import ItemCreate, ItemUpdate, ItemInDB
+from apps.schemas.item import ItemCreate, ItemUpdate, ItemGet
 from sqlalchemy.orm import Session
-from core.secure import get_hashed_password, verify_password
-
+from sqlalchemy.exc import IntegrityError
+from fastapi.encoders import jsonable_encoder
+from datetime import datetime
 
 import uuid
 
 
 class CRUDItem(CRUDBase[Item, ItemCreate, ItemUpdate]):
-    def create(self, db: Session, *, obj_in: ItemCreate) -> Item:
-        obj_db = Item(
-            uid=uuid.uuid4().hex,
+    def create(self, db: Session, *, obj_in: ItemCreate) -> ItemGet:
+        db_obj = Item(
+            owner_id=obj_in.owner_id,
+            title=obj_in.title,
+            itemdata=obj_in.startupdata
+        )
+        db_obj.created = datetime.utcnow()
+        db_obj.edited = datetime.utcnow()
 
+        new_id = uuid.uuid4().hex
+
+        while db.query(Item).filter(Item.id == new_id).first():
+            new_id = uuid.uuid4().hex
+
+        db_obj.id = new_id
+
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+
+        item = ItemGet(
+            id=db_obj.id,
+            created=db_obj.created,
+            edited=db_obj.edited,
+            owner_id=db_obj.owner_id,
+            title=db_obj.title,
+            itemdata=db_obj.itemdata
         )
 
-        db.add(obj_db)
-        db.commit()
-        db.refresh(obj_db)
-        return obj_db
+        return item
 
-    def get_by_email(self, db: Session, *, email: str) -> Optional[User]:
-        return db.query(User).filter(User.email == email).first()
+    def get_by_id(self, db: Session, id: str) -> ItemGet:
+        db_obj = self.get(db, id)
+        return ItemGet.from_orm(db_obj) if db_obj else None
 
-    def update(
-        self, db: Session, *, db_obj: User, obj_in: Union[UserUpdate, Dict[str, Any]]
-    ) -> User:
+    # def get_by_owner(self, db: Session, owner: str, skip: int = 0, limit: int = 100) -> Optional[List[ItemGet]]:
+    #     return db.query(Item).filter(Item.owner_id == owner).offset(skip).limit(limit).all()
+
+    def get_by_owner(self, db: Session, owner: str) -> Optional[List[ItemGet]]:
+        db_items = db.query(Item).filter(Item.owner_id == owner).all()
+        items = [ItemGet.from_orm(item) for item in db_items]
+        return items or None
+
+    def update(self, db: Session, *, db_obj: Item, obj_in: Union[ItemUpdate, Dict[str, Any]]) -> ItemGet:
         if isinstance(obj_in, dict):
             update_data = obj_in
         else:
             update_data = obj_in.dict(exclude_unset=True)
-        if update_data["password"]:
-            hashed_password = get_hashed_password(update_data["password"])
-            del update_data["password"]
-            update_data["hashed_password"] = hashed_password
+        update_data["updated"] = datetime.utcnow()
         return super().update(db, db_obj=db_obj, obj_in=update_data)
 
-    def authenticate(self, db: Session, *, email: str, password: str = None) -> Optional[User]:
-        user = self.get_by_email(db, email=email)
-        if not user:
-            return None
-        if not verify_password(password, user.hashed_password):
-            return None
-        return user
-
-    def is_active(self, user: User) -> bool:
-        return user.is_active
-
-#     def is_superuser(self, user: User) -> bool:
-#         return user.is_superuser
+    def delete(self, db: Session, *, item_id: str) -> ItemGet:
+        db_obj = self.get(db, item_id)
+        if not db_obj:
+            raise ValueError(f"Item with id '{item_id}' not found")
+        item = ItemGet.from_orm(db_obj)
+        db.delete(db_obj)
+        db.commit()
+        return item
 
 
-user = CRUDUser(User)
+item = CRUDItem(Item)
--- apps/crud/crud_user.py
+++ apps/crud/crud_user.py
@@ -1,3 +1,5 @@
+import uuid
+
 from typing import (
     Union,
     Optional,
@@ -6,31 +8,31 @@ from typing import (
 )
 
 from .crud_base import CRUDBase
-from apps.models.user import User
-from apps.schemas.user import UserCreate, UserUpdate, UserType
 from sqlalchemy.orm import Session
 from apps.core.security import get_hashed_password, verify_password
+from apps.models.user import User
+from apps.schemas.user import UserCreate, UserUpdate, UserGet, UserType
 
 
 class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
-    def create(self, db: Session, *, obj_in: UserCreate) -> User:
+    def create(self, db: Session, *, obj_in: UserCreate) -> UserGet:
         db_obj = User(
+            id=uuid.uuid4().hex,
             email=obj_in.email,
-            type=True if obj_in.user_type == UserType.app else False,
+            type=obj_in.user_type,
             displayname=obj_in.displayname,
-            hashed_password=get_hashed_password(obj_in.password)
+            hashed_password=get_hashed_password(
+                obj_in.password) if obj_in.user_type == UserType.app else None
         )
         db.add(db_obj)
         db.commit()
         db.refresh(db_obj)
-        return db_obj
+        return UserGet(id=db_obj.id, displayname=db_obj.displayname, email=db_obj.email, user_type=db_obj.type)
 
     def get_by_email(self, db: Session, *, email: str) -> Optional[User]:
         return db.query(User).filter(User.email == email).first()
 
-    def update(
-        self, db: Session, *, db_obj: User, obj_in: Union[UserUpdate, Dict[str, Any]]
-    ) -> User:
+    def update(self, db: Session, *, db_obj: User, obj_in: Union[UserUpdate, Dict[str, Any]]) -> UserGet:
         if isinstance(obj_in, dict):
             update_data = obj_in
         else:
@@ -41,13 +43,13 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
             update_data["hashed_password"] = hashed_password
         return super().update(db, db_obj=db_obj, obj_in=update_data)
 
-    def authenticate(self, db: Session, *, email: str, password: str = None) -> Optional[User]:
+    def authenticate(self, db: Session, *, email: str, password: str = None) -> Optional[UserGet]:
         user = self.get_by_email(db, email=email)
         if not user:
             return None
         if not verify_password(password, user.hashed_password) and user.type is UserType.app:
             return None
-        return user
+        return UserGet(displayname=user.displayname, email=user.email, user_type=user.type)
 
 
 user = CRUDUser(User)
