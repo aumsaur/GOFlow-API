5b1fa6d28f4f983190e39cc0884088f3cc49a7da
AUMSTER <74236586+aumsaur@users.noreply.github.com>
2023-03-29 21:05:37 +0700

    connect db

--- apps/azure/session.py
+++ apps/azure/session.py
@@ -1,9 +1,14 @@
+import urllib
+import pyodbc
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from .base import Base
+from apps.core.credentials import azure_setting
 
-engine = create_engine("sqlite:///mockup_azure_sql.db",
-                       connect_args={"check_same_thread": False})
+engine = create_engine(azure_setting.CONNECT_STR)
+
+# engine = create_engine("sqlite:///mockup_azure_sql.db",
+#                        connect_args={"check_same_thread": False})
 
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
