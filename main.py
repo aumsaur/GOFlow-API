934542284b09fa985147eb9f42ad50b57dd2bcf7
AUMSTER <74236586+aumsaur@users.noreply.github.com>
2023-03-29 18:14:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from typing import List
-
+import uvicorn
 from fastapi import FastAPI
 
 # from apps.api.v1.auth.service import router
-# from apps.core.config import settings
+from apps.core.config import settings
 from apps.api.v1.api import api_router
 from starlette.middleware.sessions import SessionMiddleware
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.add_middleware(SessionMiddleware, secret_key="!secret")
@@ -21,3 +21,12 @@ app.add_middleware(SessionMiddleware, secret_key="!secret")
 #     )
 
 app.include_router(api_router)
+
+if __name__ == '__main__':
+    uvicorn.run("main:app",
+                host="localhost",
+                port=8000,
+                reload=True,
+                ssl_keyfile="./key.pem",
+                ssl_certfile="./cert.pem"
+                )
