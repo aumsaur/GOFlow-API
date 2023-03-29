03dfc693c0d6680ca0bbd0c3cbecbdf87fc33906
AUMSTER <74236586+aumsaur@users.noreply.github.com>
2023-03-29 18:14:48 +0700

    Update base_model.py

--- apps/azure/base_model.py
+++ apps/azure/base_model.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import as_declarative, declared_attr
 
 @as_declarative()
 class Base:
-    id: Any
+    id: str
     __name__: str
     # Generate __tablename__ automatically
 
