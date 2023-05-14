598fff963d3888c823f233632ca55f99d4525d88
AUMSTER <74236586+aumsaur@users.noreply.github.com>
2023-05-14 19:53:09 +0700

    Update config.py

--- apps/core/config.py
+++ apps/core/config.py
@@ -5,7 +5,6 @@ from .credentials import app_setting, google_setting
 class Settings(BaseSettings):
     API_V1_STR = "/api/v1"
     BACKEND_CORS_ORIGINS = [
-        "https://localhost:8000",
         "http://localhost:3000",
     ]
 
@@ -13,16 +12,16 @@ class Settings(BaseSettings):
     ALGORITHM = app_setting.ALGORITHM
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8  # 8 days
 
-    CLIENT_ID = google_setting.CLIENT_ID
-    PROJECT_ID = "goflow-fastapi"
-    AUTH_URI = "https://accounts.google.com/o/oauth2/auth"
-    TOKEN_URI = "https://oauth2.googleapis.com/token"
-    AUTH_PROVIDER_X509_CERT_URL = "https://www.googleapis.com/oauth2/v1/certs"
-    CLIENT_SECRET = google_setting.CLIENT_SECRET
-    REDIRECT_URIS = [
-        "https://localhost:8000/user/login/access-token/google/callback"
-    ]
-    JAVASCRIPT_ORIGINS = ["https://localhost:8000"]
+    # CLIENT_ID = google_setting.CLIENT_ID
+    # PROJECT_ID = "goflow-fastapi"
+    # AUTH_URI = "https://accounts.google.com/o/oauth2/auth"
+    # TOKEN_URI = "https://oauth2.googleapis.com/token"
+    # AUTH_PROVIDER_X509_CERT_URL = "https://www.googleapis.com/oauth2/v1/certs"
+    # CLIENT_SECRET = google_setting.CLIENT_SECRET
+    # REDIRECT_URIS = [
+    #     "https://localhost:8000/user/login/access-token/google/callback"
+    # ]
+    # JAVASCRIPT_ORIGINS = ["https://localhost:8000"]
 
     class Config:
         case_sensitive = True
