55596f3cd018dcf542ccd5bf9269adabefef2d0e
AUMSTER <74236586+aumsaur@users.noreply.github.com>
2023-05-14 19:52:50 +0700

    Update service.py
    - add authentication

--- apps/api/v1/item/service.py
+++ apps/api/v1/item/service.py
@@ -1,6 +1,7 @@
-from typing import List
+import json
+from typing import Any, List
 
-from fastapi import APIRouter, HTTPException, Request, Depends
+from fastapi import APIRouter, HTTPException, Request, status, Depends
 from sqlalchemy.orm import Session
 
 
@@ -13,63 +14,64 @@ router = APIRouter()
 
 
 @router.post("/create", response_model=schemas.ItemGet)
-def create_item(db: Session = Depends(deps.get_db), *, item: schemas.ItemCreate):
-    return crud.item.create(db=db, obj_in=item)
+def create_item(request: Request, db: Session = Depends(deps.get_db), *, item: schemas.ItemCreate):
+    user_id = request.state.token_sub.get('id')
+    return crud.item.create(db=db, obj_in=item, owner_id=user_id)
 
 
 @router.get("/read/{item_id}", response_model=schemas.ItemGet)
-def read_item(db: Session = Depends(deps.get_db), *, item_id: str):
-    # TODO: authenticate user
+def read_item(request: Request, db: Session = Depends(deps.get_db), *, item_id: str):
     db_item = crud.item.get(db=db, id=item_id)
+    user_id = request.state.token_sub.get('id')
     if db_item is None:
         raise HTTPException(status_code=404, detail="Item not found")
+    if db_item.owner_id != user_id:
+        raise HTTPException(status_code=401, detail="Unauthorized")
     return db_item
 
 
-@router.get("/fetch/{owner_id}", response_model=schemas.ItemGet)
-def fetch_item(db: Session = Depends(deps.get_db), *, owner_id: str):
-    # def fetch_item(db: Session = Depends(deps.get_db), *, owner_id: str, access_token: dict):
-    # TODO: authenticate user
-    # checking if token expire
-    # if (verify_access_token(access_token)):
-    # return  # unauthentucated TODO: re-authenticated
-    # get owner id from token
-
-    #
-    owned_item = crud.item.get_by_owner(db=db, owner=owner_id)
+@router.get("/fetch", response_model=List[schemas.ItemGet])
+def fetch_item(request: Request, db: Session = Depends(deps.get_db)):
+    user_id = request.state.token_sub.get('id')
+    owned_item = crud.item.get_by_owner(db=db, owner=user_id)
     if owned_item is None:
         raise HTTPException(status_code=404, detail="Item not found")
     return owned_item
 
 
-@router.put("/update/{item_id}", response_model=schemas.ItemGet)
-def update_item(db: Session = Depends(deps.get_db), *, item_id: str, item: schemas.ItemUpdate):
-    # TODO: authenticate
+@router.put("/update", response_model=schemas.ItemGet)
+def update_item(request: Request, db: Session = Depends(deps.get_db), *, item_id: str, item: schemas.ItemUpdate):
     db_item = crud.item.get(db=db, id=item_id)
     if db_item is None:
         raise HTTPException(status_code=404, detail="Item not found")
+
     return crud.item.update(db=db, db_obj=db_item, obj_in=item)
 
 
-@router.delete("/delete/{item_id}", response_model=schemas.ItemGet)
-def delete_item(item_id: str, db: Session = Depends(deps.get_db)):
-    # TODO: authenticate
+@router.delete("/delete", response_model=schemas.ItemGet)
+def delete_item(request: Request, db: Session = Depends(deps.get_db), *, item_id: str):
+    user_id = request.state.token_sub.get('id')
     db_item = crud.item.get(db=db, id=item_id)
     if db_item is None:
         raise HTTPException(status_code=404, detail="Item not found")
+    if db_item.owner_id != user_id:  # not owner
+        raise HTTPException(
+            status_code=403, detail="You do not have permission to delete this item")
     return crud.item.delete(db=db, item_id=item_id)
 
 
-@router.get("/fetch/all", response_model=List[schemas.ItemGet])
+@router.get("/fetch/all", response_model=List[schemas.ItemGet], tags=["Debugs"])
 def fetch_all_items(db: Session = Depends(deps.get_db), *, skip: int = 0, limit: int = 100):
     items = crud.item.get_multi(db, skip=skip, limit=limit)
     return items
-# {
-# "access_token": create_access_token(
-#     {"sub": user.json()}, expires_delta = access_token_expires),
-# "token_type": "bearer", "status": status.HTTP_200_OK
-# }
-#
-# TODO: authenticate by
-# pass access token to parameter then decode in backend
-# or remember session in backend then use the session to authenticate
+
+
+@router.get("/fetch/relation", tags=["Debugs"])
+def fetch_relation(request: Request, item_id: str, db: Session = Depends(deps.get_db)):
+    db_item = crud.item.get(db=db, id=item_id)
+    owner_id = request.state.token_sub.get('id')
+    if db_item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    if db_item.owner_id == owner_id:  # Have permission to manipulate
+        print(db_item.owner_id + ' ; ' + owner_id)
+    return db_item.owner
